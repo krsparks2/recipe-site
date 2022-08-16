73dbdfd1f937e42062affe496ea84363c3da8b93
Keli <krsparks2@yahoo.com>
2022-08-15 19:19:01 -0700

    delete button removal

--- public/js/profile.js
+++ public/js/profile.js
@@ -24,26 +24,26 @@ const newFormHandler = async (event) => {
   }
 };
 
-const delButtonHandler = async (event) => {
-  if (event.target.hasAttribute('data-id')) {
-    const id = event.target.getAttribute('data-id');
+// const delButtonHandler = async (event) => {
+//   if (event.target.hasAttribute('data-id')) {
+//     const id = event.target.getAttribute('data-id');
 
-    const response = await fetch(`/api/recipes/${id}`, {
-      method: 'DELETE',
-    });
+//     const response = await fetch(`/api/recipes/${id}`, {
+//       method: 'DELETE',
+//     });
 
-    if (response.ok) {
-      document.location.replace('/profile');
-    } else {
-      alert('Failed to delete recipe');
-    }
-  }
-};
+//     if (response.ok) {
+//       document.location.replace('/profile');
+//     } else {
+//       alert('Failed to delete recipe');
+//     }
+//   }
+// };
 
 document
   .querySelector('.new-recipe-form')
   .addEventListener('submit', newFormHandler);
 
-document
-  .querySelector('.recipe-list')
-  .addEventListener('click', delButtonHandler);
+// document
+//   .querySelector('.recipe-list')
+//   .addEventListener('click', delButtonHandler);
